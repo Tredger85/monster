85c374cde2401d1d4c5df917c07dbedcb30a6f65
Andrew Franko <andrewhfrank@gmail.com>
2022-09-08 10:24:41 -0700

    Saving before blowing away defult page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: { firstName: 'Andrew', lastName: 'Franko'}
+        }
+    }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Hi {this.state.name.firstName} {this.state.name.lastName}
+            </p>
+            <button
+                onClick={()=>{
+                    this.setState(()=>{
+                        return {
+                            name: {firstName: 'Tori', lastName: 'Overman'},
+                        };
+                    },()=>{});
+                }}> Change Name </button>
+          </header>
+        </div>
+
+    );
+  }
 }
 
 export default App;
